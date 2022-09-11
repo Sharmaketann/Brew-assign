f10bbf1085cacc9ea6c4cfff53da7fa51a81ed3d
Sharma Ketan <sharmaketann@gmail.com>
2022-09-11 13:41:34 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './components/Navbar/navbar'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello Brew Apps</h1>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbar.css'
+function Navbar() {
+  return (
+    <nav>
+      <img
+        className='nav--logo'
+        src='https://www.thebrewapps.com/wp-content/uploads/2021/08/cropped-Logo1-156x80.jpg'
+        alt='airbnb logo'
+      />
+    </nav>
+  )
+}
+
+export default Navbar
